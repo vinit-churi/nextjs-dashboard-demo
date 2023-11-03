778306203612f712c4593f5c6783ba8426e37862
Vinit Churi <vinitchuri@hotmail.com>
2023-11-03 15:31:59 +0530

    chapter 13

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,27 @@
-'use client';
-
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-
+"use client";
+import { useRef } from "react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 export default function Search({ placeholder }: { placeholder: string }) {
+  const { replace } = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const intervalRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  function handleSearch(query: string) {
+    const params = new URLSearchParams(searchParams);
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+    intervalRef.current = setTimeout(() => {
+      params.set("page", "1");
+      if (query) {
+        params.set("query", query);
+      } else {
+        params.delete("query");
+      }
+      replace(`${pathname}?${params.toString()}`);
+    }, 2000);
+  }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +30,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get("query")?.toString() ?? ""}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
